2e6a668f7340330c64bcd958f4008456d6acc408
LucksGun <luckgun99@gmail.com>
2025-08-07 03:35:33 +0000

    iusdf

--- src/InterviewMode.tsx
+++ src/InterviewMode.tsx
@@ -8,7 +8,7 @@ import './InterviewMode.css'; // Make sure this CSS file exists
 
 // --- Constants ---
 const INTERVIEWER_PERSONA_ID = 'interviewer';
-const MAX_RECORDING_DURATION = 55000; // 55 seconds for user to speak
+const MAX_RECORDING_DURATION = 40000; // 55 seconds for user to speak
 
 // GOOGLE_STT_ENCODING_MAP to map browser MIME types to Google STT encodings
 const GOOGLE_STT_ENCODING_MAP: Record<string, string> = {
