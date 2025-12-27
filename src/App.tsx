c725da74b1beb8d5d62d6432a9cc040676d17ec4
LucksGun <luckgun99@gmail.com>
2025-12-27 12:07:50 +0000

    errr

--- src/App.tsx
+++ src/App.tsx
@@ -322,7 +322,7 @@ function MainApp() {
         return 'en-US';
     });
     const [selectedPersona, setSelectedPersona] = useState<Persona>(DEFAULT_UNRESTRICTED_PERSONA);
-    const [isInterviewModeOpen, setIsInterviewModeOpen] = useState(false);
+    const [isInterviewModeOpen, setIsInterviewModeOpen] = useState(true);
     const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
     const [isAdvancedSettingsOpen, setIsAdvancedSettingsOpen] = useState<boolean>(false);
     const [isStaffLoginModalVisible, setIsStaffLoginModalVisible] = useState<boolean>(false);
