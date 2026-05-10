50f419fe3d967c807cf64a1c6cf718564eed1df6
Thammaluck Ariyathammanit <luckgun99@gmail.com>
2026-05-10 14:56:09 +0000

    jdf

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -6,13 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAhbxpAHqtZ62QSytUKW9ZwcUIOeh76DEc",
-  authDomain: "project-theraphy.firebaseapp.com",
-  databaseURL: "https://project-theraphy-default-rtdb.firebaseio.com",
-  projectId: "project-theraphy",
-  storageBucket: "project-theraphy.appspot.com",
-  messagingSenderId: "674828852767",
-  appId: "1:674828852767:web:5ddcb6accb5c681325bf2a"
+ // [REDACTED]
 };
 
 // Initialize Firebase
